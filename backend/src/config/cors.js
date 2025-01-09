225a1a6242b366aa9fa45f6f0db8b21c7eff91cc
Lucas Leite <85315619+kh1psky@users.noreply.github.com>
2025-01-09 18:31:09 -0300

    Delete backend directory

--- backend/src/config/cors.js
+++
@@ -1,9 +0,0 @@
-// config/cors.js
-const corsOptions = {
-    origin: process.env.FRONTEND_URL,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-  };
-  
-  module.exports = corsOptions;
